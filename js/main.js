372ed9a084e6de659e063cd407cae173e3323c46
j-swaroop <jyothiswaroop654@gmail.com>
2026-02-14 21:53:25 +0530

    feat: added contenteditable attr for certain elements

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-import './components/products.js';
+import "./components/products.js";
+
+const editableElements = document.querySelectorAll('[contenteditable="true"]');
+
+editableElements.forEach((element) => {
+  element.addEventListener("blur", () => {
+    if (element.innerText.trim() === "") {
+      element.textContent = "Enter Text Here";
+    }
+  });
+});
